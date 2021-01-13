e1089e06276dd6c901bd7ae68b8fd114feb10497
dev-yyh <dev-yyh>
2021-01-13 14:17:13 +0900

    add dynamic_array_stack.c

---
+++ DS/stack/dynamic_array_stack.c
@@ -0,0 +1,70 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct ArrayStack {
+  int top;
+  int capacity;
+  int* array;
+} Stack;
+
+/* 주어진 capacity 크기로 stack을 생성하는 함수 */
+Stack* createStack(int capacity) {
+  Stack* stack = (Stack*)malloc(sizeof(Stack));
+  if (!stack)
+    return NULL;
+  stack->capacity = capacity;
+  stack->top = -1;
+  stack->array = (int*)malloc(stack->capacity * sizeof(int));
+  return stack;
+}
+
+/* 스택이 비어있는지 확인하는 함수. */
+int isEmpty (Stack* stack) {
+  return (stack->top == -1);
+}
+
+/* 스택이 가득찼는지 확인하는 함수. */
+int isFull (Stack* stack) {
+  return (stack->top == stack->capacity - 1);
+}
+
+/* top 위치에 있는 값을 가져오는 함수 */
+int peek (Stack* stack) {
+  if(isEmpty(stack))
+    return INT_MIN;  
+  return stack->array[stack->top];
+}
+
+/* 스택에 값을 추가하는 함수. 스택이 가득찼을 경우 배열 크를 2배로 증가시 */
+void push(Stack *stack, int data) {
+  if (isFull(stack)) { /* stack이 가득 찼다면 크기를 2배로 늘립니다. */
+    stack->capacity *= 2; 
+    stack->array = realloc(stack->array, stack->capacity);
+  } 
+  /* top을 1증가시키고 값을 top에 저장함 */
+  stack->array[++stack->top] = data;
+  printf("%d pushed to stack\n", data);
+}
+
+/* 스택에 값을 삭제하는 함수 */
+int pop(Stack* stack) {
+  if (isEmpty(stack)) {
+    printf("Stack is empty");
+    return INT_MIN;
+  }
+  else /* top의 값을 1 감소시킴 */
+    return (stack->array[stack->top--]);
+}
+
+void main() 
+{
+  Stack* stack = createStack(3);
+  
+  push(stack, 1);
+  push(stack, 2);
+  push(stack, 3);
+  push(stack, 4);
+  
+  printf("%d poped from stack\n", pop(stack));
+}
